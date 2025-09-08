d6b410bf3dade20188308f9e695408954b96c883
FrankArtn <frank_artnarong@hotmail.com>
2025-09-08 14:51:22 +1000

    migration fix for render to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Don’t bundle optional DB drivers; they’ll only be required at runtime
+      // if you flip DB_PROVIDER later.
+      config.externals = config.externals || [];
+      config.externals.push({
+        pg: "commonjs pg",
+        "better-sqlite3": "commonjs better-sqlite3",
+      });
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
